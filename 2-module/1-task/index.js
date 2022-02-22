d45cab152361f14cc77ca8af0bf68c850f7e4132
Valentina Voronicheva <valentina.voronicheva@genestack.com>
2022-02-22 14:10:58 +0300

    Module 2

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,10 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let sum = 0;
+  for (let salary in salaries) {
+    const currentSalary = salaries[salary];
+    if ((typeof currentSalary === "number") && (isFinite(currentSalary))) {
+      sum = sum + currentSalary;
+    }
+  }
+  return sum;
 }
